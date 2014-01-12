1dab47055eef54421c18b12d68505c9ddfadc035
ogama8 <ogama8@gmail.com>
2014-01-12 00:31:20 -0800

    added assignment1 source

---
+++ assignment1/assignment1.py
@@ -0,0 +1,51 @@
+###############################################################
+# Eli Backer - Jan 2014 - assignment1.py
+# This script takes in a WAV file and randomly repeats bits
+# of it.  That sound is then rewritten to the source directory
+# with "_REWRITE" attached to the end of the file name.
+###############################################################
+
+import sys
+import math
+import wave
+import random
+import progressbar
+
+
+REPEAT_VALS   = [2, 3, 4, 6, 8, 12, 16]
+RANDOM_CHANCE = .5
+RESET_RATE    = 2
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+    	fileName = sys.argv[1]
+	
+	inWAV    = wave.open(fileName, 'r')
+	wavLen   = inWAV.getnframes()
+	sampRate = inWAV.getframerate()
+	inLen	 = int(math.floor(wavLen/sampRate))
+
+	outWAV = wave.open(fileName[:-4] + '_REWRITE.wav', 'w')
+	outWAV.setnchannels(inWAV.getnchannels())
+	outWAV.setframerate(inWAV.getframerate())
+	outWAV.setsampwidth(inWAV.getsampwidth())
+
+	bar = progressbar.ProgressBar(maxval=inLen, \
+            widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+
+	for i in range(0, inLen):
+		if (i % RESET_RATE == 0):
+			inWAV.setpos(i * sampRate)
+
+		repeatDivision = REPEAT_VALS[random.randint(0, len(REPEAT_VALS) - 1)]
+		sampLen        = sampRate/repeatDivision
+
+		for j in range(0,repeatDivision):
+			if (random.random() < RANDOM_CHANCE  and  i != 0):
+				inWAV.setpos(inWAV.tell() - sampLen)
+			outWAV.writeframesraw(inWAV.readframes(sampLen))
+		bar.update(i)
+    
+	outWAV.close()
+    bar.finish()
